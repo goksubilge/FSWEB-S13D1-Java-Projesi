c75fdbff73581a6c68687b1c0dee3ce491d6b5bd
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-09-24 02:18:51 +0300

    all questions complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import org.w3c.dom.ls.LSOutput;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("_____HAVLAYAN KOPUS_____");
@@ -15,21 +14,43 @@ public class Main {
         System.out.println(isCatPlaying(true, 10));
         System.out.println(isCatPlaying(false, 36));
         System.out.println(isCatPlaying(false, 35));
-        System.out.println("_____ALAN HESAPLAMA_____");
+        System.out.println("_____ALAN HESAPLAMA_1__Dikdörtgen_____");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please give a value 1");
+        double  value1 = scan.nextDouble();
+        System.out.println("please give a value 2");
+        double value2 = scan.nextDouble();
+        System.out.println("alan = " + area(value1, value2));
+        System.out.println("_____ALAN HESAPLAMA_2__Daire_____");
+        System.out.println("please give a value");
+        double value = scan.nextDouble();
+        System.out.println("alan = " + String.format("%.5f",area(value)));
     }
-    public static boolean shouldWakeUp (boolean isHav, int clock ) {
+    public static boolean shouldWakeUp(boolean isHav, int clock ) {
         if (clock <0 || clock>23) return false;
         if (!isHav) return false;
         return clock <8 || clock>=20;
     }
-    public static boolean hasTeen (int age1, int age2, int age3){
+    public static boolean hasTeen(int age1, int age2, int age3){
         return age1 > 13 && age1 <= 19 || age2 > 13 && age2 <= 19 || age3 > 13 && age3 <= 19;
     }
-    public static boolean isCatPlaying (boolean isSummer, int temperature) {
+    public static boolean isCatPlaying(boolean isSummer, int temperature) {
         if (!isSummer) return temperature >= 25 && temperature <= 35;
         else return temperature >= 25 && temperature <= 45;
     }
-    public static
+    public static double area(double bir, double iki ) {
+        if(bir <0  || iki <0) {
+            System.out.println("You must give a positive integer!");
+            return -1;
+        }
+        return bir * iki;
+    }
 
-
+    public static double area(double radius) {
+        if(radius <0) {
+            System.out.println("You must give a positive integer!");
+            return -1;
+        }
+        return radius*radius*Math.PI;
+    }
 }
